96b8e87ba200956966a0aee30f545bc015e104d2
Andres <andreseguanipa1@gmail.com>
2021-11-25 22:04:51 -0400

    DONE

---
+++ process/guardar.php
@@ -0,0 +1,39 @@
+<?php
+
+if(isset($_POST['save'])){
+    if($_POST['save'] == "salvar"){
+        if(isset($_POST['directorio']) && isset($_POST['nota']) && isset($_POST['valor-nota'])){
+
+            $dir = $_POST['directorio'];
+            $note = $_POST['nota'];
+            $content = $_POST['valor-nota'];
+
+            try{
+
+                $file = "..\\archivos\\" . $dir . '\\' . $note;
+                $gestor = fopen($file, "w");
+
+                //...
+
+                fwrite($gestor, $content);
+                fclose($gestor);
+
+                header('Location: ../directorio.php?dir=' . $dir);
+
+            }catch (Exception $e){
+                echo 'Excepción capturada: ',  $e->getMessage(), "\n\n";
+            }
+
+
+        }else {
+            header('Location: ../index.php');
+        }
+    }else {
+        header('Location: ../index.php');
+    }
+}else {
+    header('Location: ../index.php');
+}
+
+
+?>
